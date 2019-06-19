7375dc5f97537ae6c65d32b1b1316c26d5f60bf2
Овчинникова Аделина <deley@mail.ru>
2019-06-19 19:30:17 +0500

    Changes in  program

--- PointOnChart/PointOnChart/Program.cs
+++ PointOnChart/PointOnChart/Program.cs
@@ -9,17 +9,25 @@ namespace PointOnChart
 {
     class Program
     {
+        private static double x, y;//координаты точки
+        private static bool Rezult()//для результата
+        {
+            bool ok = false;
+            if (y - x / 2 <= 0 && x * x + y * y <= 1)
+                ok = true;
+            return ok;
+        }
         static void Main()
         {
-            Color.Print("\n Введите координаты точки:\n", ConsoleColor.Magenta);
-            Color.Print(" X = ", ConsoleColor.Green);
-            double x = Number.Check(-100.0, 100.0);
-            Color.Print(" Y = ", ConsoleColor.Green);
-            double y = Number.Check(-100.0, 100.0);
-            if (y - x / 2 <= 0 && x * x + y * y <= 1)
-                Color.Print(" Зашло" + "(" + x + " ; " + y + ") " + "\n u = -3", ConsoleColor.Green);
+            Color.Print("\n Введите координаты точки:\n", ConsoleColor.Green);
+            Color.Print(" X = ", ConsoleColor.Yellow);
+            x = Number.Check(-100.0, 100.0);
+            Color.Print(" Y = ", ConsoleColor.Yellow);
+            y = Number.Check(-100.0, 100.0);
+            if (Rezult())
+                Color.Print(" Точка " + "(" + x + " ; " + y + ") принадлежит области." + "\n u = -3", ConsoleColor.Green);
             else
-                Color.Print(" Вылетела " + "(" + x + " ; " + y + ") u = " + y * y, ConsoleColor.Red);
+                Color.Print(" Точка " + "(" + x + " ; " + y + ") не принадлежит облатси. \n u = " + y * y, ConsoleColor.Red);
             Text.GoBackMenu();
         }
     }
